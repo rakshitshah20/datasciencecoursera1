4dccf2b389652c9ca0fca0c515a294b3ee5592f2
Rakshit Shah <rakshitshahsrt@gmail.com>
2021-02-21 18:09:28 +0530

    first commit

---
+++ my_first_r_script.R
@@ -0,0 +1,6 @@
+1+1
+demo()
+example <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8), nrow = 4, ncol = 2)
+View(example)
+savehistory("C:/Rakshit/RStudio/my_first_R.Rhistory")
+savehistory("C:/Rakshit/RStudio/New folder/my_first_r_script.R")
